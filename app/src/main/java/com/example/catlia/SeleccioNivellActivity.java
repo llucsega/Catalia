a19a36855323f2fd1875c60e6906e3bf523bfae0
llucs <llucsegala29@gmail.com>
2025-11-20 16:34:29 +0100

    Segona versió de Catalia, eric treballa

---
+++ app/src/main/java/com/example/catlia/SeleccioNivellActivity.java
@@ -0,0 +1,12 @@
+package com.example.catlia;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SeleccioNivellActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccio_nivell);
+    }
+}
